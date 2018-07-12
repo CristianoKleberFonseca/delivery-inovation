25798ab4055143920964bee08a519f96f5727d2e
Cristiano Fonseca <cristiano.fonseca@selfitacademias.com.br>
2018-07-11 21:08:56 -0300

    Criacao de projeto para manipulacao da entidade usuario

---
+++ inovation-crud-user-server/src/main/java/br/com/mv/inovation/InovationCrudUserServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.mv.inovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InovationCrudUserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InovationCrudUserServerApplication.class, args);
+	}
+}
